d7973b284760e59ed32600a93a6690d745735310
Steve Lipton <steve@makeapppie.com>
2019-01-08 18:11:29 -0600

    Initial Commit

---
+++ UnicodeCharacters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnicodeCharacters
+//
+//  Created by Steven Lipton on 1/8/19.
+//  Copyright © 2019 Steven Lipton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
